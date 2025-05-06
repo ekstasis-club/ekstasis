655a79e695182117eea21a5dfb85cd3780f2fdd9
Tu Nombre <ekstasis.club@gmail.com>
2025-05-06 17:33:16 +0200

    0.0.9

---
+++ src/app/[locale]/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>EKSTASIS</title>
+      </>
+    );
+  }
+  
