d150c111d49930ae4cf7d8bf36eb8477d746c2bd
Tu Nombre <ekstasis.club@gmail.com>
2025-05-06 18:08:41 +0200

    0.0.9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import '../styles/global.css';
 import { ReactNode } from 'react';
 
-type Props = {
-  children: ReactNode;
-};
-
-export default function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es" className="h-full bg-black">
       <body className="relative h-full min-h-screen bg-black text-white font-sans">
-        {/* Capa de fondo forzada */}
+        {/* Fondo negro fijo para Safari/iOS */}
         <div className="fixed inset-0 bg-black z-[-1]" />
-        
-        {/* Contenido */}
         {children}
       </body>
     </html>
