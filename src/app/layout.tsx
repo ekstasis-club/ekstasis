34b5ba5006f2810ae099825a5415ffefa6c68d7c
Tu Nombre <ekstasis.club@gmail.com>
2025-05-06 18:00:26 +0200

    0.0.9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
-import {ReactNode} from 'react';
+import '../styles/global.css';
+import { ReactNode } from 'react';
 
 type Props = {
   children: ReactNode;
 };
 
-// Since we have a `not-found.tsx` page on the root, a layout file
-// is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
-  return children;
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="es" className="h-full bg-black">
+      <body className="min-h-screen h-full bg-black text-white font-sans">
+        <div className="fixed inset-0 bg-black z-[-1]" />
+        {children}
+      </body>
+    </html>
+  );
 }
